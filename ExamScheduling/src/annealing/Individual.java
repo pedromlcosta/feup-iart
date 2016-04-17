7cc481bfdaa85a28fa40d33496a36da8609fb609
ruiProg <ruifilipeosorio17@hotmail.com>
2016-04-17 06:52:28 +0100

    SA alghoritm

---
+++ ExamScheduling/src/annealing/Individual.java
@@ -0,0 +1,15 @@
+package annealing;
+
+public class Individual {
+
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Individual getNeighbour() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- ExamScheduling/src/annealing/SimmulatedAnnealing.java
+++ ExamScheduling/src/annealing/SimmulatedAnnealing.java
@@ -1,5 +1,48 @@
 package annealing;
 
-public class SimmulatedAnnealing {
+public class SimulatedAnnealing {
+	
+	private static double T_EPSILON = 0.00001;
+	private double temperature;
+	private double coolingRate;
+	private Individual currentSolution;
+	private Individual newSolution;
+	private Individual bestSolution;
+	
+	SimulatedAnnealing(){
+		coolingRate = 0.97;
+	}
+	
+	SimulatedAnnealing(double coolingRate){
+		this.coolingRate = coolingRate;
+	}
 
+	public Individual search(){
+		
+		int oldCost, newCost, bestCost = 0;
+		currentSolution = new Individual();
+		oldCost = currentSolution.getValue();
+		temperature = 1;
+		
+		while(temperature > T_EPSILON){
+			newSolution = currentSolution.getNeighbour();
+			newCost = newSolution.getValue();
+			if(Math.random() < acceptNeighnour(oldCost,newCost))
+				currentSolution = newSolution;
+			
+			temperature *= coolingRate;
+			
+			if(newCost > bestCost){
+				bestSolution = newSolution;
+				bestCost = newCost;
+			}
+		}
+		
+		return bestSolution;
+	}
+	
+	double acceptNeighnour(int oldCost, int newCost){
+		
+		return Math.exp((newCost-oldCost)/temperature);
+	}
 }
